a766ec621c079f3ec406214688528b0b58373a72
Jc3046 <jeanalcazar26@gmail.com>
2024-03-04 16:06:21 -0600

    Conatiner v2

--- instroduccionbootstrap/obj/Debug/net6.0/instroduccionbootstrap.AssemblyInfo.cs
+++ instroduccionbootstrap/obj/Debug/net6.0/instroduccionbootstrap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("instroduccionbootstrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01fc51fa724f66db7da0f3ab28b75c354ee5662c")]
 [assembly: System.Reflection.AssemblyProductAttribute("instroduccionbootstrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("instroduccionbootstrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
